33252e5cf7ead96e20241a4f6a60b36d463b0ee1
Maik Wildermann <web-developer-mw@outlook.de[B>
2025-06-12 08:53:22 +0200

    tailwind.config.js erweitert, fonts, colors, pluggin, picture und favicon hinzugefügt

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/partials/home.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primaryfarbe für Headers, Buttons, Links
+        primary: {
+          default: "#1A2A47", // dunkelblau
+          light: "#435B7A", // für Hover-Zustände
+          dark: "#0F1A2B", // für Footer/Hintergründe
+        },
+        // Sekundärfarbe für Akzente, Icons, CTA
+        secondary: {
+          default: "#D4AF37", // gold
+          light: "E1BE54",
+          dark: "#A8872E",
+        },
+        // Hintergrundfläche
+        background: {
+          default: "#0F1A2B", // fast schwarzblau
+          muted: "#F5F7FA", // Grauton für weniger wichtige Farbe
+        },
+        // Optional: Akzentfarbe für CTAs oder Links
+        accent: "#C49E2A", // etwas dunkleres Gold
+      },
+      fontFamily: {
+        sans: ["Montserat", "ui-sans-serif", "system-ui"],
+        serif: ["Merriweather", "ui-serif", "Georgia"],
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
